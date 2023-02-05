b9e5e35f9d45e8ab4c4e014b0350601ed6eabdc3
HASEBE Masahiko <JCA03205@gmail.com>
2023-02-05 09:13:35 +0900

    ワーニング対応

--- src/lpnlib.rs
+++ src/lpnlib.rs
@@ -15,11 +15,11 @@ pub const MAX_LEFT_PART: usize = 2;
 pub const MAX_RIGHT_PART: usize = 2;
 pub const MAX_PART: usize = MAX_LEFT_PART+MAX_RIGHT_PART;
 
-pub const FIRST_COMPOSITION_PART: usize = 0;
+pub const _FIRST_COMPOSITION_PART: usize = 0;
 pub const MAX_COMPOSITION_PART: usize = MAX_PART; // Normal と対応する同数のパート
 
-pub const FIRST_PHRASE_PART: usize = MAX_COMPOSITION_PART;
+pub const _FIRST_PHRASE_PART: usize = MAX_COMPOSITION_PART;
 pub const MAX_PHRASE_PART: usize = MAX_PART;       //Composition と対応
 
-pub const DAMPER_PEDAL_PART: usize = MAX_COMPOSITION_PART+MAX_PHRASE_PART;
+pub const _DAMPER_PEDAL_PART: usize = MAX_COMPOSITION_PART+MAX_PHRASE_PART;
 pub const MAX_PART_COUNT: usize = MAX_COMPOSITION_PART+MAX_PHRASE_PART+1;
