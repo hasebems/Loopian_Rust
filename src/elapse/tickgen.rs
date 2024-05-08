395732f6179af62464db5f71593965db4705915f
HASEBE Masahiko <JCA03205@gmail.com>
2024-05-08 21:10:18 +0900

    rit.処理のリファクタリング

--- src/elapse/tickgen.rs
+++ src/elapse/tickgen.rs
@@ -18,17 +18,9 @@ pub struct TickGen {
     crnt_msr: i32,           // start からの小節数（最初の小節からイベントを出すため、-1初期化)
     crnt_tick_inmsr: i32,    // 現在の小節内の tick 数
     crnt_time: Instant,      // 現在の時刻
-
     rit_state: bool,
-    fermata_state: bool,    // fermata で止まっている状態
-    minus_bpm_for_gui: i16, // realtime に rit. で減るテンポ
-    last_addup_tick: i32,
-    last_addup_time: Instant,
-    t0_time: f32,       // tempo=0 到達時間
-    t0_addup_tick: i32, // tempo=0 到達時の積算tick
-    delta_tps: f32,     // Tick per sec: tick の時間あたりの変化量、bpm 変化量を８倍した値
-    rit_bar: i32,       // rit 受信後、何回小節線をスルーするか
-    rit_bar_count: i32, // rit_bar を小節頭で inc.
+    fermata_state: bool, // fermata で止まっている状態
+    ritgen: RitGen,
 }
 #[derive(Clone, Copy, PartialEq)]
 pub struct CrntMsrTick {
@@ -60,17 +52,9 @@ impl TickGen {
             crnt_msr: -1,
             crnt_tick_inmsr: 0,
             crnt_time: Instant::now(),
-
             rit_state: false,
             fermata_state: false,
-            minus_bpm_for_gui: 0,
-            last_addup_tick: 0,
-            last_addup_time: Instant::now(),
-            t0_time: 0.0,
-            t0_addup_tick: 0,
-            delta_tps: 0.0,
-            rit_bar: 0,
-            rit_bar_count: 0,
+            ritgen: RitGen::new(),
         }
     }
     pub fn change_beat_event(&mut self, tick_for_onemsr: i32, beat: Beat) {
@@ -118,8 +102,28 @@ impl TickGen {
         let former_msr = self.crnt_msr;
         self.crnt_time = crnt_time;
         if self.rit_state {
-            self.calc_tick_rit(crnt_time);
+            // rit.
+            if let Some(tick_in_msr) = self.ritgen.calc_tick_rit(
+                crnt_time,
+                self.bpm_start_time,
+                self.tick_for_onemsr,
+                self.bpm,
+                self.bpm_start_tick,
+            ) {
+                self.crnt_msr += tick_in_msr / self.tick_for_onemsr;
+                self.crnt_tick_inmsr = tick_in_msr % self.tick_for_onemsr;
+            } else {
+                // rit 終了
+                self.rit_state = false;
+                self.crnt_msr = self.beat_start_msr + self.ritgen.rit_bar + 1;
+                self.crnt_tick_inmsr = 0;
+                self.beat_start_msr = self.crnt_msr;
+                self.bpm_start_time = crnt_time;
+                self.bpm_start_tick = 0;
+                self.bpm = self.bpm_stock;
+            }
         } else {
+            // same bpm
             let tick_from_beat_starts = self.calc_crnt_tick();
             self.crnt_msr =
                 (tick_from_beat_starts / self.tick_for_onemsr + self.beat_start_msr) as i32;
@@ -164,10 +168,20 @@ impl TickGen {
         self.bpm
     }
     pub fn get_real_bpm(&self) -> i16 {
-        self.bpm - self.minus_bpm_for_gui
+        self.bpm - self.ritgen.delta_bpm
     }
     pub fn get_beat(&self) -> Beat {
         self.beat
+    }
+    pub fn start_rit(&mut self, start_time: Instant, ratio: i32, bar: i32, target_bpm: i16) {
+        if ratio < 100 && !self.rit_state && !self.fermata_state {
+            self.ritgen.set_rit(ratio, bar, self.bpm as f32);
+        }
+        self.rit_state = true;
+        self.beat_start_msr = self.crnt_msr;
+        self.bpm_start_time = start_time;
+        self.bpm_start_tick = self.crnt_tick_inmsr;
+        self.bpm_stock = target_bpm;
     }
     fn calc_crnt_tick(&self) -> i32 {
         let diff = self.crnt_time - self.bpm_start_time;
@@ -175,72 +189,93 @@ impl TickGen {
             ((DEFAULT_TICK_FOR_QUARTER as f32) * (self.bpm as f32) * diff.as_secs_f32()) / 60.0;
         elapsed_tick as i32 + self.bpm_start_tick
     }
+}
 
+pub struct RitGen {
+    delta_bpm: i16,         // realtime に rit. で減るテンポ（微分値）
+    delta_tps: f32,         // Tick per sec: tick の時間あたりの変化量、bpm 変化量を８倍した値
+    rit_bar: i32,           // rit 受信後、何回小節線をスルーするか
+    rit_bar_count: i32,     // rit_bar を小節頭で inc.
+    last_addup_tick: i32,
+    last_addup_time: Instant,
+    t0_time: f32,       // tempo=0 到達時間
+    t0_addup_tick: i32, // tempo=0 到達時の積算tick
+}
+
+impl RitGen {
+    pub fn new() -> Self {
+        Self {
+            delta_bpm: 0,
+            delta_tps: 0.0,
+            rit_bar: 0,
+            rit_bar_count: 0,
+            last_addup_tick: 0,
+            last_addup_time: Instant::now(),
+            t0_time: 0.0,
+            t0_addup_tick: 0,
+        }
+    }
     //==== rit. ======================
     // ratio  0:   tempo 停止
     //        50:  1secで tempo を 50%(1/2)
     //        100: 何もしない
     // bar    0: 次の小節まで、1: 次の次の小節まで (何回小節跨ぎをスルーするか)
-    pub fn start_rit(&mut self, start_time: Instant, ratio: i32, bar: i32, target_bpm: i16) {
-        if ratio >= 100 || self.rit_state || self.fermata_state {
-            return;
-        } else {
-            self.delta_tps = ((100.0 - ratio as f32) / 100.0) * 8.0 * (self.bpm as f32);
-        }
-        self.t0_time = (self.bpm as f32) * 8.0 / self.delta_tps; // tempo0 time
+    pub fn set_rit(&mut self, ratio: i32, bar: i32, bpm: f32) {
+        self.delta_tps = ((100.0 - ratio as f32) / 100.0) * 8.0 * bpm;
+        self.t0_time = bpm * 8.0 / self.delta_tps; // tempo0 time
         self.t0_addup_tick = ((self.delta_tps / 2.0) * self.t0_time * self.t0_time) as i32;
-
-        self.rit_state = true;
-        self.beat_start_msr = self.crnt_msr;
-        self.bpm_start_time = start_time;
-        self.bpm_start_tick = self.crnt_tick_inmsr;
         self.rit_bar = bar;
         self.rit_bar_count = 0;
-        self.bpm_stock = target_bpm;
     }
-    fn calc_tick_rit(&mut self, crnt_time: Instant) {
+    pub fn calc_tick_rit(
+        &mut self,
+        crnt_time: Instant,
+        bpm_start_time: Instant,
+        tick_for_onemsr: i32,
+        original_bpm: i16,
+        bpm_start_tick: i32,
+    ) -> Option<i32> {
         // output: self.crnt_msr の更新
-        let tick_from_rit_starts = self.calc_current_tick_rit(crnt_time);
-        if self.tick_for_onemsr * (self.rit_bar + 1) < tick_from_rit_starts {
+        let tick_from_rit_starts =
+            self.calc_addup_tick_rit(crnt_time, bpm_start_time, original_bpm) + bpm_start_tick;
+        if tick_for_onemsr * (self.rit_bar + 1) < tick_from_rit_starts {
             // reached last bar, and stop rit.
-            self.rit_state = false;
-            self.crnt_msr = self.beat_start_msr + self.rit_bar + 1;
-            self.crnt_tick_inmsr = 0;
-
-            self.beat_start_msr = self.crnt_msr;
-            self.bpm_start_time = crnt_time;
-            self.bpm_start_tick = 0;
-            self.minus_bpm_for_gui = 0;
             self.rit_bar = 0;
-            self.bpm = self.bpm_stock;
+            self.delta_bpm = 0;
+            None
         } else {
-            let r_msr = tick_from_rit_starts / self.tick_for_onemsr;
-            let r_tick_inmsr = tick_from_rit_starts % self.tick_for_onemsr;
+            let r_msr = tick_from_rit_starts / tick_for_onemsr;
+            let mut r_tick_inmsr = tick_from_rit_starts % tick_for_onemsr;
             if r_msr > self.rit_bar_count {
                 // 小節線を超えたとき
                 self.rit_bar_count += 1;
-                self.crnt_msr += 1;
+                r_tick_inmsr += tick_for_onemsr;
             }
-            self.crnt_tick_inmsr = r_tick_inmsr;
+            Some(r_tick_inmsr)
         }
     }
-    fn calc_current_tick_rit(&mut self, crnt_time: Instant) -> i32 {
+    fn calc_addup_tick_rit(
+        &mut self,
+        crnt_time: Instant,
+        bpm_start_time: Instant,
+        original_bpm: i16,
+    ) -> i32 {
         const MINIMUM_TEMPO: i16 = 20;
-        let start_time = (crnt_time - self.bpm_start_time).as_secs_f32();
+        let start_time = (crnt_time - bpm_start_time).as_secs_f32();
         let time_to0 = self.t0_time - start_time;
-        self.minus_bpm_for_gui = (self.delta_tps * start_time / 8.0) as i16;
+        self.delta_bpm = (self.delta_tps * start_time / 8.0) as i16;
         let addup_tick: i32;
-        if self.bpm - self.minus_bpm_for_gui > MINIMUM_TEMPO {
+        if original_bpm - self.delta_bpm > MINIMUM_TEMPO {
             // target bpm が MINIMUM_TEMPO 以上
             addup_tick = self.t0_addup_tick - (time_to0 * time_to0 * self.delta_tps / 2.0) as i32; // 積算Tickの算出
             self.last_addup_tick = addup_tick;
             self.last_addup_time = crnt_time;
         } else {
-            self.minus_bpm_for_gui = self.bpm - MINIMUM_TEMPO;
+            self.delta_bpm = original_bpm - MINIMUM_TEMPO;
             addup_tick = self.last_addup_tick
                 + (8.0 * (MINIMUM_TEMPO as f32) * (crnt_time - self.last_addup_time).as_secs_f32())
                     as i32;
         }
-        addup_tick + self.bpm_start_tick // 現在の tick
+        addup_tick
     }
 }
