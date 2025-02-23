66cd45b2bcf64eec0e4fe97171ef4d40df146dea
HASEBE Masahiko <JCA03205@gmail.com>
2025-02-23 19:57:53 +0900

    beatlissaを機能追加し、大幅変更

--- src/graphic/beatlissa.rs
+++ src/graphic/beatlissa.rs
@@ -4,6 +4,7 @@
 //  https://opensource.org/licenses/mit-license.php
 //
 use nannou::prelude::*;
+use rand::Rng;
 
 use super::draw_graph::Resize;
 use super::generative_view::*;
@@ -16,33 +17,37 @@ pub struct BeatLissa {
     measure_position: i32,
     max_obj_inline: i32,
     max_obj: i32,
-    obj_locate: Vec<Vec2>,
+    obj_locate: Vec<Vec2>,       // Vec2: 位置
     bobj: Vec<Box<dyn BeatObj>>, // Beat Object
     mode: GraphMode,
 }
 //*******************************************************************
+const SQUARE_BIG_SIZE: f32 = 230.0;
 const SQUARE_SIZE: f32 = 100.0;
 const GAP: f32 = 180.0;
 const MAX_LINE: i32 = 2;
 const START_Y: f32 = 150.0;
 impl BeatLissa {
-    pub fn new(beat_inmsr: i32, _tm: f32, mode: GraphMode) -> Self {
+    pub fn new(beat_inmsr: i32, _tm: f32, blmd: i32, mode: GraphMode) -> Self {
         //self.max_obj_inline = beat_inmsr;
-        let max_obj = beat_inmsr * MAX_LINE;
-        let start_x = -(beat_inmsr as f32 * SQUARE_SIZE + (beat_inmsr - 1) as f32 * GAP) / 2.0
-            + SQUARE_SIZE / 2.0;
-        let start_y = START_Y;
         let mut obj_locate: Vec<Vec2> = Vec::new();
-        for i in 0..max_obj {
-            let xnum = i % beat_inmsr;
-            let ynum = (i / beat_inmsr) as f32;
-            let x = (SQUARE_SIZE + GAP) * xnum as f32 + start_x;
-            let y = start_y - ynum * (SQUARE_SIZE + GAP);
-            obj_locate.push(Vec2::new(x, y));
+        let max_obj: i32;
+        if blmd == 0 {
+            max_obj = 1;
+        } else {
+            max_obj = beat_inmsr * MAX_LINE;
+            let start_x = -(beat_inmsr as f32 * SQUARE_SIZE + (beat_inmsr - 1) as f32 * GAP) / 2.0
+                + SQUARE_SIZE / 2.0;
+            let start_y = START_Y;
+
+            for i in 0..max_obj {
+                let xnum = i % beat_inmsr;
+                let ynum = (i / beat_inmsr) as f32;
+                let x = (SQUARE_SIZE + GAP) * xnum as f32 + start_x;
+                let y = start_y - ynum * (SQUARE_SIZE + GAP);
+                obj_locate.push(Vec2::new(x, y));
+            }
         }
-        //println!(
-        //    "QQQ::beat_inmsr:{}, x:{}",beat_inmsr, start_x
-        //);
         Self {
             beat: 0,
             measure_position: 0,
@@ -76,16 +81,36 @@ impl GenerativeView for BeatLissa {
         if bt == 0 {
             self.measure_position += 1;
         }
-        let max_obj =
-            ((self.measure_position * self.max_obj_inline + self.beat) % self.max_obj + 1) as usize;
+        if !self.obj_locate.is_empty() {
+            let max_obj = ((self.measure_position * self.max_obj_inline + self.beat) % self.max_obj
+                + 1) as usize;
 
-        if max_obj <= 1 && self.bobj.len() > max_obj {
-            self.bobj.clear();
-        }
-        if self.bobj.len() < max_obj {
-            let loc = self.obj_locate[self.bobj.len()];
-            self.bobj
-                .push(Box::new(BeatLissaObj::new(tm, dt, loc.x, loc.y, self.mode)));
+            if max_obj <= 1 && self.bobj.len() > max_obj {
+                self.bobj.clear();
+            }
+            if self.bobj.len() < max_obj {
+                let loc = self.obj_locate[self.bobj.len()];
+                self.bobj.push(Box::new(BeatLissaObj::new(
+                    tm,
+                    dt,
+                    loc.x,
+                    loc.y,
+                    SQUARE_SIZE,
+                    self.mode,
+                )));
+            }
+        } else if bt == 0 {
+            if !self.bobj.is_empty() {
+                self.bobj.clear();
+            };
+            self.bobj.push(Box::new(BeatLissaObj::new(
+                tm,
+                dt * self.max_obj_inline as f32,
+                0.0,
+                0.0,
+                SQUARE_BIG_SIZE,
+                self.mode,
+            )));
         }
     }
     /// Mode 情報を受け取る
@@ -115,35 +140,67 @@ pub struct BeatLissaObj {
     draw_time: f32,
     polyline: Vec<Vec2>,
     center: Vec2,
+    size: f32,
+    thickness: f32,
     mode: GraphMode,
+
+    phase_x: f32,
+    phase_y: f32,
+    ratio_x: f32,
+    ratio_y: f32,
 }
 //*******************************************************************
+const DRAW_SPEED: f32 = 12.0;
+//const X_PHASE: f32 = 1.4;
+//const Y_PHASE: f32 = 0.6;
+//const X_RATIO: f32 = 0.78;
+//const Y_RATIO: f32 = 1.35;
+//*******************************************************************
 impl BeatLissaObj {
-    pub fn new(crnt_time: f32, draw_time: f32, x: f32, y: f32, mode: GraphMode) -> Self {
+    pub fn new(
+        crnt_time: f32,
+        draw_time: f32,
+        center_x: f32,
+        center_y: f32,
+        size: f32,
+        mode: GraphMode,
+    ) -> Self {
+        let thickness = if size == SQUARE_BIG_SIZE { 5.0 } else { 4.0 };
+        let mut rng = rand::thread_rng();
         Self {
-            phase: crnt_time * 2.0,
+            phase: crnt_time * DRAW_SPEED,
             first_time: crnt_time,
             last_time: 0.0,
             draw_time,
             polyline: Vec::new(),
-            center: Vec2::new(x, y),
+            center: Vec2::new(center_x, center_y),
+            size,
+            thickness,
             mode,
+            phase_x: rng.gen::<f32>() + 0.5,
+            phase_y: rng.gen::<f32>() + 0.5,
+            ratio_x: rng.gen::<f32>() + 0.5,
+            ratio_y: rng.gen::<f32>() + 0.5,
         }
     }
 }
-//*******************************************************************
-const DRAW_SPEED: f32 = 12.0;
 impl BeatObj for BeatLissaObj {
     /// Beat の Model の更新
     fn update_model(&mut self, crnt_time: f32, _rs: Resize) -> bool {
         if crnt_time - self.last_time > 0.01 && crnt_time - self.first_time < self.draw_time {
             // 10msec ごとに更新、draw_time で終了
             let new_phase = crnt_time * DRAW_SPEED;
-            let mut position: Vec2 = Default::default();
-            position.x = ((new_phase + 1.4) * 0.78).sin() * SQUARE_SIZE + self.center.x;
-            position.y = ((new_phase + 0.6) * 1.35).sin() * SQUARE_SIZE + self.center.y;
-            self.polyline.push(position);
-            self.phase = new_phase;
+            let mut fine_phase = self.phase;
+            while fine_phase < new_phase {
+                let mut position: Vec2 = Default::default();
+                position.x =
+                    ((fine_phase + self.phase_x) * self.ratio_x).sin() * self.size + self.center.x;
+                position.y =
+                    ((fine_phase + self.phase_y) * self.ratio_y).sin() * self.size + self.center.y;
+                self.polyline.push(position);
+                fine_phase += 0.1;
+            }
+            self.phase = fine_phase;
             self.last_time = crnt_time;
         }
         true
@@ -159,21 +216,18 @@ impl BeatObj for BeatLissaObj {
         if num < 2 {
             return;
         }
-        let color = if self.mode == GraphMode::Dark {
-            WHITE
+        let gray = if self.mode == GraphMode::Dark {
+            1.0
         } else {
-            BLACK
+            0.0
         };
-        draw.line()
-            .start(self.polyline[1])
-            .end(self.polyline[0])
-            .weight(3.0)
-            .color(MAGENTA);
-        for i in 1..num - 1 {
+        for i in 0..num - 1 {
+            let alpha_level = (i as f32) / (num as f32);
+            let color = rgba(gray, gray, gray, alpha_level);
             draw.line()
                 .start(self.polyline[i + 1])
                 .end(self.polyline[i])
-                .weight(3.0)
+                .weight(self.thickness)
                 .color(color);
         }
     }
