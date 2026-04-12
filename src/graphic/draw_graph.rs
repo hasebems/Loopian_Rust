6b99f047325e1c11b2095d61a22f3a97a432a05f
HASEBE Masahiko <JCA03205@gmail.com>
2026-04-12 17:50:01 +0900

    scrolltextの位置を真ん中に寄せた

--- src/graphic/draw_graph.rs
+++ src/graphic/draw_graph.rs
@@ -29,20 +29,20 @@ pub struct Resize {
 impl Resize {
     pub fn new(app: &App) -> Resize {
         const INPUT_TXT_LOWER_MARGIN: f32 = 100.0; // input text
-        const MIN_LEFT_MARGIN: f32 = 140.0;
+        const MIN_LEFT_MARGIN: f32 = 0.0;
         const MIN_RIGHT_MARGIN: f32 = 30.0;
 
         let win = app.main_window();
         let win_rect = win.rect();
         let win_width = win_rect.w();
         let win_height = win_rect.h();
-        let st_left_margin = -win_width / 2.0 + MIN_LEFT_MARGIN;
+        let st_left_margin = -win_width / 6.0 + MIN_LEFT_MARGIN;
 
         Resize {
             full_size_x: win_width,
             full_size_y: win_height,
             eight_indic_left: win_width / 2.0 - MIN_RIGHT_MARGIN,
-            scroll_txt_left: st_left_margin,
+            scroll_txt_left: st_left_margin,    // 真ん中の少し左寄り
             input_txt_top: -win_height / 2.0 + INPUT_TXT_LOWER_MARGIN,
             input_txt_left: 0.0,
         }
@@ -580,8 +580,7 @@ impl Graphic {
         }
         const LINE_THICKNESS: f32 = 2.0;
         const SCRTXT_FONT_SIZE: u32 = 18;
-        const SPACE2_TXT_LEFT_MARGIN: f32 = 40.0;
-        const UNDERLINE_POS_ADJ_X: f32 = 60.0;
+        const UNDERLINE_POS_ADJ_X: f32 = -100.0;
         const UNDERLINE_POS_ADJ_Y: f32 = 14.0;
 
         // Draw Letters
@@ -617,7 +616,7 @@ impl Graphic {
                         (Self::CURSOR_GRAY as f32 * dissapiering) as u8 / alpha,
                     ))
                     .x_y(
-                        self.rs.scroll_txt_left + center_adjust - UNDERLINE_POS_ADJ_X,
+                        self.rs.scroll_txt_left + center_adjust + UNDERLINE_POS_ADJ_X,
                         scroll_txt_bottom
                             + Graphic::SCRTXT_FONT_HEIGHT * ((max_lines - 1 - i) as f32)
                             - UNDERLINE_POS_ADJ_Y,
@@ -645,7 +644,6 @@ impl Graphic {
                     .left_justify()
                     .x_y(
                         self.rs.scroll_txt_left
-                            + SPACE2_TXT_LEFT_MARGIN
                             + Graphic::SCRTXT_FONT_WIDTH * (j as f32),
                         scroll_txt_bottom
                             + Graphic::SCRTXT_FONT_HEIGHT * ((max_lines - 1 - i) as f32),
